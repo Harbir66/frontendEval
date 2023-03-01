ba9675eb78546ae41b067547c2c10818e02e5db4
Harbir Singh <harbir066@gmail.com>
2023-03-01 15:57:43 +0530

    feat : add Error Page

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Footer, Header } from '../../components';
+import './error.css';
+
+function Error() {
+  const { errorCode } = useParams();
+  return (
+    <div className="main">
+      <Header />
+      <div className="errorContainer">
+        <p>Something went wrong!</p>
+        {errorCode && <p>{`Error code: ${errorCode}`}</p>}
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Error;
