74795beb1982a0fc82febfb6e577967d21121f13
Harbir Singh <harbir066@gmail.com>
2023-03-01 15:56:17 +0530

    feat : add Main Container for Event Cards

---
+++ src/components/MainContainer/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './MainContainer.css';
+import { useNavigate } from 'react-router-dom';
+import FilterBar from '../FilterBar';
+import makeRequest from '../../utils/makeRequest';
+import { GET_ALL_EVENTS } from '../../constants/apiEndPoints';
+import EventCard from '../EventCard';
+
+function MainContainer() {
+  const [events, setEvents] = React.useState();
+  const [error, setError] = React.useState();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    makeRequest(GET_ALL_EVENTS, {}, navigate)
+      .then((data) => {
+        setEvents(data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, []);
+
+  if (error) {
+    return (
+      <div className="main-container padding">
+        <FilterBar />
+        <div>{error.message}</div>
+      </div>
+    );
+  }
+  return (
+    <div className="main-container padding">
+      <FilterBar />
+      {events ? (
+        <div className="events-container">
+          {events.map((event) => {
+            return <EventCard key={event.id} event={event} />;
+          })}
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
+}
+
+export default MainContainer;
